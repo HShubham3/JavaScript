aae659b4d39f80479bea20e9602a2ccb0ee4a1a1
Shubham_Hadole <129657078+HShubham3@users.noreply.github.com>
2025-12-06 08:16:37 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+// Data types //
+
+/*
+Premitive dataType
+1. int
+2. bigInt
+3. string
+4. boolean
+5. null
+6. undifined
+7. symbol ==> to define uniqueness
+
+Non-Premitive dataType
+1. object 
+*/
+
+let age
+console.log(typeof age) // undifined
+age = 10;
+console.log(typeof age) // number
+age = 10.0;
+console.log(typeof (age)) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undifined
