727115560c14bd8283261ae1f754ac6d554288e6
Shubham_Hadole <129657078+HShubham3@users.noreply.github.com>
2025-12-06 08:17:05 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let count = 35
+
+// console.log(count)
+// console.log(typeof count);
+// console.log(typeof(count))
+
+
+// count = "45"
+// console.log(typeof count);
+// let numberCount1 = Number(count)
+// console.log(numberCount1)
+
+// count = "55qgfk"
+// console.log(count)
+// console.log(typeof count);
+// let numberCount2 = Number(count)
+// console.log(numberCount2) // NaN not a number
+
+// null
+count = null
+console.log(count)
+console.log(typeof count);
+console.log(Number(count))
+
+// undifined
+count = undefined
+console.log(count)
+console.log(typeof count);
+console.log(Number(count))
+
+// boolean
+count = true
+console.log(count)
+console.log(typeof count);
+console.log(Number(count))
+
+console.log("-------------------------------- Boolean ---------------------------------------------")
+
+let number = 10;
+console.log(Boolean(number))
+
+number = 0
+console.log(Boolean(number))
+
+number = 0.1
+console.log(Boolean(number))
+
+
+
